e7d7383f9795ee515b5d1e43c18bbdfa1bb336b6
JORGE ARMANDO GUZMAN FLORES <A01208480@itesm.mx>
2018-02-06 21:23:12 -0600

    lab3

---
+++ app/src/main/java/com/example/jorge/lab3/ChildActivity.java
@@ -0,0 +1,82 @@
+package com.example.jorge.lab3;
+
+import android.app.NotificationManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.app.PendingIntent;
+import android.support.v7.app.NotificationCompat;
+
+
+
+public class ChildActivity extends AppCompatActivity {
+
+    Context context;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_child);
+        Intent targetIntent = getIntent();
+        String username = targetIntent.getStringExtra("username");
+        String pass = targetIntent.getStringExtra("pass");
+        Button buttonBack = (Button) findViewById(R.id.buttonBack);
+        int NOTIF_ID = 1234;
+
+        buttonBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent buttonBack = new Intent(ChildActivity.this, MainActivity.class);
+                startActivity(buttonBack);
+            }
+        });
+
+        if (username.equals("karnal") && pass.equals("pass")) {
+            AlertDialog.Builder DialogConf = new AlertDialog.Builder(this);
+            DialogConf.setTitle("Welcome" + username);
+            DialogConf.setMessage("Hello World");
+            DialogConf.setIcon(R.mipmap.ic_launcher);
+            AlertDialog MyDialog = DialogConf.create();
+            MyDialog.show();
+
+            //getnotification(NOTIF_ID);
+        } else {
+            AlertDialog.Builder DialogConf = new AlertDialog.Builder(this);
+            DialogConf.setTitle("Invalid");
+            DialogConf.setMessage("Invalid credentials");
+            DialogConf.setIcon(R.mipmap.ic_launcher);
+            DialogConf.setNeutralButton("Retry",
+                    new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+                            Intent main = new Intent(ChildActivity.this, MainActivity.class);
+                            startActivity(main);
+                        }
+                    });
+
+            AlertDialog MyDialog = DialogConf.create();
+            MyDialog.show();
+        }
+    }
+
+    public void getnotification(int NOTIF_ID){
+        NotificationCompat.Builder NotifBuilder = new NotificationCompat.Builder(this);
+        NotifBuilder.setSmallIcon(R.mipmap.ic_launcher);
+        NotifBuilder.setContentTitle("Important Notification");
+        NotifBuilder.setContentText("This is the detail of the notification");
+
+        Intent notificationIntent = new Intent(this, ChildActivity.class);
+        notificationIntent.putExtra("myData", "This string comes from the previous activity");
+        PendingIntent contentIntent = PendingIntent.getActivity(context, 0, notificationIntent, 0);
+
+        NotifBuilder.setContentIntent(contentIntent);
+
+        NotificationManager MyNotification = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        MyNotification.notify(NOTIF_ID, NotifBuilder.build());
+    }
+}
--- app/src/main/java/com/example/jorge/lab3/MainActivity.java
+++ app/src/main/java/com/example/jorge/lab3/MainActivity.java
@@ -2,12 +2,41 @@ package com.example.jorge.lab3;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.content.Context;
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.support.v7.app.NotificationCompat;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        context = getApplicationContext();
+        Button submit = (Button) findViewById(R.id.submit);
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                TextView password = (TextView) findViewById(R.id.password);
+                TextView user = (TextView) findViewById(R.id.user);
+                String username = user.getText().toString();;
+                String pass =  password.getText().toString();;
+                Toast MyToast = new Toast(context);
+                MyToast.makeText(context, "Redirecting", Toast.LENGTH_LONG).show();
+                Intent view2 = new Intent(MainActivity.this, ChildActivity.class);
+                view2.putExtra("username", username);
+                view2.putExtra("pass", pass);
+                startActivity(view2);
+            }
+        });
     }
 }
